59bcc11eaf59ee8203ca2bd107db400eb80c8d66
박창조 <ckdwh1202@gmail.com>
2025-10-27 15:46:25 +0900

    feat(car): add car domain

---
+++ src/domain/Car.js
@@ -0,0 +1,30 @@
+import OutputView from '../view/OutputView';
+
+class Car {
+  #name;
+  #position;
+
+  constructor(name) {
+    this.#name = name;
+    this.#position = 0;
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  get position() {
+    return this.#position;
+  }
+
+  move(randomNumber) {
+    if (randomNumber >= 4) this.#position++;
+  }
+
+  printStatus() {
+    const status = '-'.repeat(this.#position);
+    OutputView.printMessage(`${this.#name} : ${status}\n`);
+  }
+}
+
+export default Car;
