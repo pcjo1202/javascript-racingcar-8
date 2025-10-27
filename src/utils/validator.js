0a5509026061de32cd739bc6bcb1ccd04c872690
박창조 <ckdwh1202@gmail.com>
2025-10-27 16:01:21 +0900

    feat(validator): update validator validateName method

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -3,14 +3,21 @@ import ERROR_MESSAGE from '../constants/error-message';
 class Validator {
   // 자동차 이름 검증
   static validateName(name) {
+    const nameList = name.split(','); // 배열로 변환
+
     // 이름이 비어있을 때
-    if (name.length === 0) {
+    if (nameList.length === 0) {
       throw new Error(ERROR_MESSAGE.CAR_NAME_EMPTY);
     }
     // 이름이 5자 이상일 때
-    if (name.length > 5) {
-      throw new Error(ERROR_MESSAGE.CAR_NAME_LENGTH);
-    }
+    nameList.forEach((name) => {
+      if (name.length > 5) {
+        throw new Error(ERROR_MESSAGE.CAR_NAME_LENGTH);
+      }
+    });
+
+    // 중복 체크
+    this.validateNamesDuplication(nameList);
   }
 
   // 자동자 이름 중복 체크
